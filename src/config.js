3cfdcded1b604cbae4c48333238edb32a6d68952
lojzatran <lam.tran@commercetools.com>
2022-02-14 11:23:04 +0100

    Add option for rc file config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 import { fileURLToPath } from 'url'
 import path from 'path'
 import { promises as fs } from 'node:fs'
+import rc from 'rc'
 
 function getLogLevel () {
   return process.env.LOG_LEVEL || 'info'
@@ -15,24 +16,40 @@ async function getPackageJson () {
 }
 
 function getConcurrency () {
+  const config = loadConfigFromFile()
   return {
-    ctpConcurrency: parseInt(process.env.CTP_CONCURRENCY, 10) || 10,
-    sftpConcurrency: parseInt(process.env.SFTP_CONCURRENCY, 10) || 5,
+    ctpConcurrency: parseInt(process.env.CTP_CONCURRENCY || config.ctpConcurrency, 10) || 10
   }
 }
 
 function getClientConfig () {
+  const config = loadConfigFromFile()
   return {
-    projectKey: process.env.CTP_PROJECT_KEY,
-    clientId: process.env.CTP_CLIENT_ID,
-    clientSecret: process.env.CTP_CLIENT_SECRET,
+    projectKey: process.env.CTP_PROJECT_KEY || config.ctpProjectKey,
+    clientId: process.env.CTP_CLIENT_ID || config.ctpClientId,
+    clientSecret: process.env.CTP_CLIENT_SECRET || config.ctpClientSecret,
     apiUrl:
-      process.env.CTP_API_URL
+      (process.env.CTP_API_URL || config.ctpApiUrl)
       || 'https://api.europe-west1.gcp.commercetools.com',
     authUrl:
-      process.env.CTP_AUTH_URL
+      (process.env.CTP_AUTH_URL || config.ctpAuthUrl)
       || 'https://auth.europe-west1.gcp.commercetools.com',
   }
+}
+
+let config
+let isFileLoaded
+
+function loadConfigFromFile () {
+  if (!isFileLoaded) {
+    /*
+    see: https://github.com/dominictarr/rc#standards for file precedence.
+     */
+    const appName = 'subscriptions'
+    config = rc(appName)
+    isFileLoaded = true
+  }
+  return config
 }
 
 export {
