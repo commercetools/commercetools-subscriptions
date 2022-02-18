64f5ea5c0ba67bd483b381a23c3dd19e9e239899
lojzatran <lam.tran@commercetools.com>
2022-02-18 11:29:54 +0100

    Update formatting

--- src/setup/setup.js
+++ src/setup/setup.js
@@ -3,7 +3,7 @@ import { ensureStates } from './ensure-states.js'
 import getApiRoot from '../utils/client.js'
 import getLogger from '../utils/logger.js'
 
-export default async function setupExtensionResources () {
+export default async function setupExtensionResources() {
   const apiRoot = getApiRoot()
   const mainLogger = getLogger()
   await ensureCustomTypes(apiRoot, mainLogger)
